33e11ffd6c8eb497c72468f8f779690c7b865a65
fei <feicheung2016@gmail.com>
2020-01-23 19:24:29 +0800

    Change batch size to reduce memory usage

--- myconfig.py
+++ myconfig.py
@@ -69,8 +69,8 @@ THROTTLE_REVERSE_PWM = 220      #pwm value for max reverse throttle
 # #The DEFAULT_MODEL_TYPE will choose which model will be created at training time. This chooses
 # #between different neural network designs. You can override this setting by passing the command
 # #line parameter --type to the python manage.py train and drive commands.
-DEFAULT_MODEL_TYPE = 'imu2'   #(linear|categorical|rnn|imu|behavior|3d|localizer|latent)
-# BATCH_SIZE = 128                #how many records to use when doing one pass of gradient decent. Use a smaller number if your gpu is running out of memory.
+DEFAULT_MODEL_TYPE = 'rnn_imu'   #(linear|categorical|rnn|imu|behavior|3d|localizer|latent)
+BATCH_SIZE = 64                #how many records to use when doing one pass of gradient decent. Use a smaller number if your gpu is running out of memory.
 # TRAIN_TEST_SPLIT = 0.8          #what percent of records to use for training. the remaining used for validation.
 # MAX_EPOCHS = 100                #how many times to visit all records of your data
 # SHOW_PLOT = True                #would you like to see a pop up display of final loss?
